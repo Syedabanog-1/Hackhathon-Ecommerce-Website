f8f41940c86030049a4f4a150fe967b09c872ffe
syedabanog-1 <gulzar_bano@yahoo.com>
2024-12-25 02:13:23 +0500

    commit saved ha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import NewProducts from "./components/NewProducts";
 import Footer from "./components/Footer";
 import HeaderTop from "./components/HeaderTop";
 import HeaderMin from "./components/HeaderMin";
-//import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 
 
 export default function Home() {
@@ -15,6 +15,7 @@ export default function Home() {
     <>
      <HeaderTop />
       <HeaderMin />
+      <Navbar />
       
       <Hero />
       <Image className="rounded-xl object-cover w-1/2 max-h-[700px] sm:max-h-[500px] md:max-h-[400px] lg:max-h-[500px] object-center transition-transform duration-300 ease-in-out"
