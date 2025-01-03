6fbb532247f208185d1a28a9076ada66c4cc838a
syedabanog-1 <gulzar_bano@yahoo.com>
2025-01-03 20:33:50 +0500

    commoit ha ab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import NewProducts from "./components/NewProducts";
 
 
+
 export default function Home() {
   return (
     <>
@@ -24,4 +25,3 @@ export default function Home() {
   );
 }
       
-   
